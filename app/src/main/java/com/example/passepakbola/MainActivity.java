b393bb20684e58ffdd8d6884e5cb5e9de72f794f
AbyanAgtasaniRPL1 <byan.10rpl1.2019@gmail.com>
2020-12-08 11:54:14 +0700

    Initial commit

---
+++ app/src/main/java/com/example/passepakbola/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.passepakbola;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.media.ExifInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
+
+    EditText edt1;
+    EditText edt2;
+    Button btnMasuk;
+    Button btnTutup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        pref = getSharedPreferences("login", MODE_PRIVATE);
+        edt1 = (EditText) findViewById(R.id.edit1);
+        edt2 = (EditText) findViewById(R.id.edit2);
+        btnMasuk = (Button) findViewById(R.id.btnMasuk);
+        btnMasuk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (edt1.getText().toString().isEmpty() ||
+                        edt2.getText().toString().isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Harus terisi semua", Toast.LENGTH_SHORT).show();
+
+                } else {
+
+                    if (edt1.getText().toString().equals("admin")
+                            && edt2.getText().toString().equals("admin")) {
+                        Toast.makeText(MainActivity.this, "Login sukses", Toast.LENGTH_SHORT).show();
+                        Intent i = new Intent(MainActivity.this, MainMenu.class);
+                        startActivity(i);
+
+                    } else {
+                        Toast.makeText(MainActivity.this,"Login gagal", Toast.LENGTH_SHORT) .show();
+                        finish();
+                    }
+                    //saving ke SP
+                    editor = pref.edit();
+                    editor.putString("username", edt1.getText().toString());
+                    editor.putString("status", "login");
+                    editor.apply();
+                    //menuju ke main menu
+                    startActivity(new Intent(getApplicationContext(), MainMenu.class));
+                    finish();
+                }
+
+            }
+        });
+
+    }
+}
